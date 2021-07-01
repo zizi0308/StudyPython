098314f0efe0351a29ac8e54aa3ad02ccc294d46
JO HUI JI <whgmlwl222@naver.com>
2021-07-01 17:05:51 +0900

    20210701 파일열기 학습중 커밋

--- Chap06/문자열_ex.py
+++ Chap06/문자열_ex.py
@@ -1,1 +1,15 @@
-# TODO
+# string 문자 파싱하기
+# 문자열 잘라서 숫자만 float으로 형변환 시켜서 출력하기
+
+str = 'X-DSPAM-Confidence: 0.8475'
+
+str1 = str.find(':')
+print(str1)
+
+num = str[str1+2 :]
+print(num)
+
+fval = float(num)
+
+print(fval)
+print(fval + 42.0)
---
+++ Chap07/파일열기.py
@@ -0,0 +1,4 @@
+# Reading File
+
+# Using Open >> open(파일명, 파일을 읽을지 쓸지 선택하는 모드)
+# handle = open(Filename, mode)   # open 함수가 파일 핸들을 반환, 지정한 변수인 handle에 저장
