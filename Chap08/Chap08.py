0efad7496186f304d5a36336f7722df5084d408a
JO HUI JI <whgmlwl222@naver.com>
2021-07-21 17:51:26 +0900

    20210721 리스트의 개념과 특징학습

---
+++ Chap08/Chap08.py
@@ -0,0 +1,23 @@
+
+# List are Mutable
+
+lotto = [2, 14, 35, 34, 25]
+print(lotto)
+
+lotto[2] = 13   # 2번 방에 있는 원소를 바꿈
+print(lotto)
+
+# How Long is a List?
+
+greet = 'Hello Bob!'
+print(len(greet))
+
+x = [1, 2, 'joe', 66]
+print(len(x))
+
+# Using the range function
+
+friends = ['Joseph', 'Lee', 'Sally', 'Jenny']
+print(len(friends))
+print(range(len(friends)))
+
